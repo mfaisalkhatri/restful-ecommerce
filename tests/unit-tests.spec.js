ffb8d7ee84bb9351e9bfede7095f1fb41b6447f0
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-09-04 18:42:27 +0300

    unit tests addition wip

--- tests/unit-tests.spec.js
+++ tests/unit-tests.spec.js
@@ -2,20 +2,22 @@ import request from 'supertest';
 import orders from '../testdata/neworders.json' with { type: "json" };
 import orderObject from '../testdata/neworder_only_object.json' with { type: "json" }
 import missingFieldsOrder from '../testdata/neworder_field_missing.json' with { type: "json" }
+import authCredentials from '../testdata/auth_credentials.json' with {type: "json"}
+import updateOrder from '../testdata/update_order.json' with {type: "json"}
 import {expect} from 'chai';
 
 
 describe('Unit Tests of E-Commerce application', () => {
 	const baseurl = 'http://localhost:3004';
 
-	it('should show 404 status code as no order exists', async() => {
-		let response = await request(baseurl).get('/getAllOrders').send(orderObject);
+	it('should return status code 404 as no order exists', async() => {
+		let response = await request(baseurl).get('/getAllOrders');
 		expect(response.statusCode).to.be.equal(404)
 		expect(response.body.message).to.be.equal('No Order found!!');
 
 	});
 
-	it('should show 400 status code for invalid order format', async() => {
+	it('should return status code 400 for invalid order format', async() => {
 
 		let response = await request(baseurl).post('/addOrder').send(orderObject);
 		expect(response.statusCode).to.be.equal(400)
@@ -23,14 +25,14 @@ describe('Unit Tests of E-Commerce application', () => {
 		 
 	});
 
-	it('should show 400 status code with message for fields missing in order', async() => {
+	it('should return status code 400 with message for fields missing in order', async() => {
 
 		let response = await request(baseurl).post('/addOrder').send(missingFieldsOrder);
 		expect(response.statusCode).to.be.equal(400)
 		expect(response.body.message).to.be.equal('Each order must have user_id, product_id, product_name, product_amount, qty, tax_amt, and total_amt!');
 	});
 
-	it('should create orders with 201 status code', async() => {
+	it('should create orders and return status code 201', async() => {
 
 		let response = await request(baseurl).post('/addOrder').send(orders);
 		expect(response.statusCode).to.be.equal(201)
@@ -44,7 +46,7 @@ describe('Unit Tests of E-Commerce application', () => {
 		expect(response.body.orders[1].total_amt).to.be.equal(706.99);    
 	});
 
-	it('should get all available orders successfully with 200 status', async() => {
+	it('should get all available orders successfully with status code 200', async() => {
 	
 		let response = await request(baseurl).get('/getAllOrders');
 		expect(response.statusCode).to.be.equal(200);
@@ -59,7 +61,7 @@ describe('Unit Tests of E-Commerce application', () => {
 		expect(response.body.orders[0].total_amt).to.be.equal(505.99);    
 	});
 
-	it('should filter and fetch order on user_id', async() => {
+	it('should filter and fetch order on order id with status code 200', async() => {
 		let response = await request(baseurl).get('/getOrder').query({id: 1});
 		expect(response.statusCode).to.be.equal(200);
 		expect(response.body.message).to.be.equal('Order found!!');
@@ -71,9 +73,148 @@ describe('Unit Tests of E-Commerce application', () => {
 		expect(response.body.orders[0].qty).to.be.equal(1);
 		expect(response.body.orders[0].tax_amt).to.be.equal(5.99);
 		expect(response.body.orders[0].total_amt).to.be.equal(505.99);    
+	});
 
+	it('should filter and fetch order on product_id with status code 200', async() => {
+		let response = await request(baseurl).get('/getOrder').query({product_id: 3});
+		expect(response.statusCode).to.be.equal(200);
+		expect(response.body.message).to.be.equal('Order found!!');
+		expect(response.body.orders.length).to.be.above(0);
+		expect(response.body.orders[0].user_id).to.be.equal('3');
+		expect(response.body.orders[0].product_id).to.be.equal('3');
+		expect(response.body.orders[0].product_name).to.be.equal('Samsung S24 Ultra');
+		expect(response.body.orders[0].product_amount).to.be.equal(4300);
+		expect(response.body.orders[0].qty).to.be.equal(1);
+		expect(response.body.orders[0].tax_amt).to.be.equal(5.99);
+		expect(response.body.orders[0].total_amt).to.be.equal(4305.99);    
+	});
 
+	it('should filter and fetch order on user_id with status code 200', async() => {
+		let response = await request(baseurl).get('/getOrder').query({user_id: 1});
+		expect(response.statusCode).to.be.equal(200);
+		expect(response.body.message).to.be.equal('Order found!!');
+		expect(response.body.orders.length).to.be.above(0);
+		expect(response.body.orders[1].user_id).to.be.equal('1');
+		expect(response.body.orders[1].product_id).to.be.equal('2');
+		expect(response.body.orders[1].product_name).to.be.equal('iPad');
+		expect(response.body.orders[1].product_amount).to.be.equal(699);
+		expect(response.body.orders[1].qty).to.be.equal(1);
+		expect(response.body.orders[1].tax_amt).to.be.equal(7.99);
+		expect(response.body.orders[1].total_amt).to.be.equal(706.99);
+	});
 
+	it('should return 404 status code when query no records are found for the order id filter', async() => {
+		let response = await request(baseurl).get('/getOrder').query({id: 6});
+		expect(response.statusCode).to.be.equal(404);
+		expect(response.body.message).to.be.equal('No Order found with the given parameters!');
+
+	});
+
+	it('should return 404 status code when query no records are found for the user id filter', async() => {
+		let response = await request(baseurl).get('/getOrder').query({user_id: 6});
+		expect(response.statusCode).to.be.equal(404);
+		expect(response.body.message).to.be.equal('No Order found with the given parameters!');
+
+	});
+
+	it('should return 404 status code when query no records are found for the product id filter', async() => {
+		let response = await request(baseurl).get('/getOrder').query({product_id: 6});
+		expect(response.statusCode).to.be.equal(404);
+		expect(response.body.message).to.be.equal('No Order found with the given parameters!');
+
+	});
+
+	it('should return the order with multiple filter queries with status code 200', async() =>{
+		let response = await request(baseurl).get('/getOrder').query({id: 3, user_id: 2, product_id: 2});
+		console.log(response);
+		expect(response.statusCode).to.be.equal(200);
+		expect(response.body.message).to.be.equal('Order found!!');
+		expect(response.body.orders.length).to.be.above(0);
+		expect(response.body.orders[0].id).to.be.equal(3);
+		expect(response.body.orders[0].user_id).to.be.equal('2');
+		expect(response.body.orders[0].product_id).to.be.equal('2');
+		expect(response.body.orders[0].product_name).to.be.equal('iPhone 15 PRO');
+		expect(response.body.orders[0].product_amount).to.be.equal(999);
+		expect(response.body.orders[0].qty).to.be.equal(2);
+		expect(response.body.orders[0].tax_amt).to.be.equal(9.99);
+		expect(response.body.orders[0].total_amt).to.be.equal(1088.99);
+
+	});
+
+	it('should return status code 404 and not return any order when multiple filter query params dont match', async() =>{
+		let response = await request(baseurl).get('/getOrder').query({id: 1, user_id: 3, product_id: 2});
+		expect(response.statusCode).to.be.equal(404);
+		expect(response.body.message).to.be.equal('No Order found with the given parameters!');	
+	});
+
+	it('should generate the valid token with status code 201 ', async() => {
+		let response = await request (baseurl).post('/auth').send(authCredentials);
+		expect(response.statusCode).to.be.equal(201);
+		expect(response.body.message).to.be.equal('Authentication Successful!');
+		expect(response.body.token).not.to.be.null;
+	});
+
+	it('should not generate the token and return status code 401 when invalid credentials are supplied ', async() => {
+		let response = await request (baseurl).post('/auth').send({username: "admin", password: "Password123"});
+		expect(response.statusCode).to.be.equal(401);
+		expect(response.body.message).to.be.equal('Authentication Failed! Invalid username or password!');
+	});
+
+	it('should not generate the token and return status code 401 when only username is provided and password is missing', async() => {
+		let response = await request (baseurl).post('/auth').send({username: "admin"});
+		expect(response.statusCode).to.be.equal(400);
+		expect(response.body.message).to.be.equal('Username and Password is required for authentication!');
+	});
+
+	it('should not generate the token and return status code 400 when only password is provided and username is missing', async() => {
+		let response = await request (baseurl).post('/auth').send({password: "pass123"});
+		expect(response.statusCode).to.be.equal(400);
+		expect(response.body.message).to.be.equal('Username and Password is required for authentication!');
+	});
+
+	it('should not generate the token and return status code 401 when username is blank', async() => {
+		let response = await request (baseurl).post('/auth').send({username: "blank", password: "admin"});
+		expect(response.statusCode).to.be.equal(401);
+		expect(response.body.message).to.be.equal('Authentication Failed! Invalid username or password!');
+	});
+
+	it('should not generate the token and return status code 401 when password is blank', async() => {
+		let response = await request (baseurl).post('/auth').send({username: "admin", password: "blank"});
+		expect(response.statusCode).to.be.equal(401);
+		expect(response.body.message).to.be.equal('Authentication Failed! Invalid username or password!');
+
+	});
+
+	it('should not generate the token and return status code 401 when both username and password is blank', async() => {
+		let response = await request (baseurl).post('/auth').send({username: "blank", password: "blank"});
+		expect(response.statusCode).to.be.equal(401);
+		expect(response.body.message).to.be.equal('Authentication Failed! Invalid username or password!');
+	});
+
+	it('should update the order successfully and return status code 200', async() => {
+
+		let authResponse = await request (baseurl).post('/auth').send(authCredentials);
+	
+		let response = await request (baseurl).put('/updateOrder/2')
+		.set('Content-Type', 'application/json')
+		.set('Authorization', authResponse.body.token)
+		.send(updateOrder);
+		expect(response.statusCode).to.be.equal(200);
+		expect(response.body.message).to.be.equal('Order updated successfully!!');
+		expect(response.body.order.user_id).to.be.equal('3');
+		expect(response.body.order.product_id).to.be.equal('7');
+		expect(response.body.order.product_name).to.be.equal('Samsung Android Television');
+		expect(response.body.order.product_amount).to.be.equal(8559.89);
+		expect(response.body.order.qty).to.be.equal(3);
+		expect(response.body.order.tax_amt).to.be.equal(1283.98);
+		expect(response.body.order.total_amt).to.be.equal(26960.98);
+	});
+
+	it('should not update the Order when authorization token is missing in the update request', async() => {
+
+		let response = await request (baseurl).put('/updateOrder/2').send(updateOrder);
+		expect(response.statusCode).to.be.equal(403);
+		expect(response.body.message).to.be.equal('Forbidden! Token is missing!');
 	});
 	
 
