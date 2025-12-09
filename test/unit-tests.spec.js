ec241891267009cc8813376f9c1803bd346c0cfc
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2025-12-09 23:22:54 +0530

    Issue fixed (#80)
    * updated static path to generate swagger-output instead of using process.cwd()
    
    * deleted swagger-output.json and updated unit tests
    
    * updated app.js to create a dir and write swagger output json to it

--- test/unit-tests.spec.js
+++ test/unit-tests.spec.js
@@ -365,7 +365,7 @@ describe('Unit Tests of E-Commerce application', () => {
 	
 
 	it('should generate swagger.json in the local dir successfully when /swagger.json endpoint is called', async()=> {
-		const path = Path.join(__dirname, "swagger-output.json");
+		const path = ("./swaggerfiles/swagger-output.json");
 		
   			if (fs.existsSync(path)) {
 			    fs.unlinkSync(path);
